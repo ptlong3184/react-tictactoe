5025718fac5b98298b43f946e580b4fd5f22b5ad
Long Pham <ptlong3184@gmail.com>
2024-10-27 09:05:19 +0700

    Initial commit for react-tictactoe project

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,32 +1,31 @@
-import { useState } from "react";
 
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-];
 
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-    function handleSelectSquare(rowIndex, colIndex) {
-        setGameBoard((prevGameBoard) => {
-            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-            return updatedBoard;
-        });
 
-        onSelectSquare();
-    }
+export default function GameBoard({ onSelectSquare, board }) {
+    
+
+    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    // function handleSelectSquare(rowIndex, colIndex) {
+    //     setGameBoard((prevGameBoard) => {
+    //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updatedBoard;
+    //     });
+
+    //     onSelectSquare();
+    // }
 
     return (
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => (
+            {board.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                                <button onClick={() => onSelectSquare(rowIndex, colIndex)} 
+                                disabled = {playerSymbol !== null }>
                                     {playerSymbol}
                                 </button>
                             </li>))}
@@ -34,5 +33,5 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
                 </li>
             ))}
         </ol>
-        );
+    );
 }
